c84abab4544b17e5240367999920446d4a111205
Lennard Ortmeyer <l.ortmeyer@shopware.com>
2025-11-19 09:02:54 +0100

    Add you should Use and adjust comment

--- home/features/shell/default.nix
+++ home/features/shell/default.nix
@@ -61,7 +61,8 @@
     plugins = [
       { name = "fzf"; src = pkgs.fishPlugins.fzf-fish.src; }
       { name = "async-prompt"; src = pkgs.fishPlugins.async-prompt; } # Make your prompt asynchronous
-      { name = "plugin-fish"; src = pkgs.fishPlugins.plugin-git; } # Make your prompt asynchronous
+      { name = "plugin-fish"; src = pkgs.fishPlugins.plugin-git; } # Git Aliases
+      { name = "fish-you-should-use"; src = pkgs.fishPlugins.fish-you-should-uses; } # Git Aliases
     ];
 
     functions = {
